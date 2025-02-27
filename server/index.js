6166ddc56cfc018766d0a77ad47b3cd18c9652f4
Cristian <crisgues8@gmail.com>
2025-02-27 18:00:42 -0400

    Inicializando repositorio

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+const pg = require('pg');
+require('dotenv').config();
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
