213d9976a2cf9734ebd158f3ba57e87f0317704c
Kendrick Neufeld <kendrickneufeld@gmail.com>
2025-03-06 18:43:26 -0500

    added front end for the results page

--- client/src/components/Results.jsx
+++ client/src/components/Results.jsx
@@ -2,56 +2,57 @@ import React from "react";
 import Header from "./Header";
 import Button from "./Button";
 import { IoCloudUploadOutline } from "react-icons/io5";
-import '../css/scan.css'
+import '../css/results.css';
+import { FaSyncAlt } from "react-icons/fa"; // Importamos el icono de recarga
 
-function Result(){
-    return(
-        <div>
-            <Header/>
-            <h1>Reconocimiento facial diseñado para identificar personas desaparecidas</h1>
-            <main className="main-scan">
-
-                <div className="drag-drop">
-                    <IoCloudUploadOutline size={130} color="gray"/>
-
-                    <h3>Drag and drop files here</h3>
-                    <span>or</span>
-
-                    <button>Browse Files</button>
+function Results() {
+    return (
+      <div>
+        <Header />
+        <h1>Reconocimiento facial diseñado para identificar personas desaparecidas</h1>
+        <main className="main-results">
+          {/* Contenedor de la imagen otorgada */}
+          <div className="image-container">
+            <h3>Imagen otorgada</h3>
+            <img src="URL_DE_LA_IMAGEN" alt="Imagen otorgada" className="result-image" />
+          </div>
+  
+          {/* Contenedor del formulario con botones e imagen extra */}
+          <div className="form-container">
+            <div className="form-content">
+              {/* Botones arriba */}
+              <div className="button-group">
+                <button className="findings-button">Hallazgos</button>
+                <button className="reload-button">
+                  <FaSyncAlt />
+                </button>
+              </div>
+  
+              {/* Imagen extra encima del formulario */}
+              <img src="URL_DE_LA_IMAGEN_EXTRA" alt="Resultados" className="extra-image" />
+  
+              {/* Formulario */}
+              <div className="form">
+                <div>
+                  <label htmlFor="nombre">Nombre:</label>
+                  <input type="text" id="nombre" />
                 </div>
-
-                <div className="form">
-                    <div>
-                        <label htmlFor="nombre">Nombre:</label>
-                        <input type="text" id="nombre" />
-                    </div>
-                    
-                    <div>
-                        <label htmlFor="ID">ID:</label>
-                        <input type="text" id="ID" />
-                    </div>
-                    
-                    <div>
-                        <label htmlFor="lastSeen">Último lugar:</label>
-                        <input type="text" id="lastSeen"/>
-                    </div>
+  
+                <div>
+                  <label htmlFor="ID">ID:</label>
+                  <input type="text" id="ID" />
                 </div>
-
-
-                <div className="sub-buttons">
-                    <div className="top-buttons">
-                        <button className="bottom-buttons">Escoger foto <img src="images/pictures.svg" alt="escoger" /></button>
-                        <button className="bottom-buttons">Tomar foto <img src="images/camera.svg" alt="tomar" /> </button>
-                    </div>
-                    
-                    <div className="search-button">
-                        <Button name="Search"/>
-                    </div>
+  
+                <div>
+                  <label htmlFor="lastSeen">Último lugar:</label>
+                  <input type="text" id="lastSeen" />
                 </div>
-
-
-            </main>
-        </div>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
     );
-}
-export default Result;
+  }
+  
+  export default Results;
